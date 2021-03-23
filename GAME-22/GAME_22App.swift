8a3548e737bc5ca29f6325b777cef57735885635
c60603 <c60603@gmail.com>
2021-03-23 18:25:49 +0800

    Initial Commit

---
+++ GAME-22/GAME_22App.swift
@@ -0,0 +1,17 @@
+//
+//  GAME_22App.swift
+//  GAME-22
+//
+//  Created by User11 on 2021/3/23.
+//
+
+import SwiftUI
+
+@main
+struct GAME_22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
